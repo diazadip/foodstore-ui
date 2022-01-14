8346906201d9fdf4ec1c8af4669be2763145f6ec
diazadip <diaz_adip@yahoo.co.id>
2022-01-14 22:19:21 +0700

    sukses isi 2

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,13 @@
-import { combineReducers, createStore, applyMiddleware, compose } from
-'redux';
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import authReducer from '../features/Auth/reducer';
+import productReducer from '../features/Products/reducer';
 
-const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-|| compose;
+const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const rootReducers = combineReducers({
- auth: authReducer
+    auth: authReducer,
+    products: productReducer
 });
 const store = createStore(rootReducers,
-composerEnhancer(applyMiddleware(thunk)));
+    composerEnhancer(applyMiddleware(thunk)));
 export default store;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,26 +1,49 @@
 import * as React from 'react';
-import { SideNav, LayoutSidebar } from 'upkit';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+    SideNav,
+    LayoutSidebar,
+    Responsive,
+    CardProduct,
+} from 'upkit';
+import TopBar from '../../components/TopBar';
 import menus from './menus';
+import { fetchProducts } from '../../features/Products/actions';
+export default function Home() {
+    let dispatch = useDispatch();
+    let products = useSelector(state => state.products);
+    React.useEffect(() => {
+        dispatch(fetchProducts());
+    }, [dispatch])
 
-export default function Home()
-{
     return (
-    <div>
-    <LayoutSidebar
-    sidebar={<SideNav items={menus} verticalAlign="top"/>}
-    content=
-    {
-        <div className="md:flex md:flex-row-reverse w-full mr-5 h-full min-h-screen">
-        <div className="w-full md:w-3/4 pl-5 pb-10">
-            Konten utama di sini
+        <div>
+            <LayoutSidebar
+                sidebar={<SideNav items={menus} verticalAlign="top" />}
+                content={<div className="md:flex md:flex-row-reverse w-full mr-5 h-full min-h-screen">
+                    <div className="w-full md:w-3/4 pl-5 pb-10">
+                        <TopBar />
+                        <Responsive desktop={3} items="stretch">
+                            {products.data.map((product, index) => {
+                                return <div key={index} className="p-2">
+                                    <CardProduct
+                                        title={product.name}
+                                        imgUrl=
+                                        {`${process.env.REACT_APP_API_HOST}/upload/${product.image_url}`}
+                                        price={product.price}
+                                        onAddToCart={_ => null}
+                                    />
+                                </div>
+                            })}
+                        </Responsive>
+                    </div>
+
+                    <div className="w-full md:w-1/4 h-full shadow-lg border-r border-white bg-gray-100">
+                        Keranjang belanja di sini
+                    </div>
+                </div>}
+                sidebarSize={80}
+            />
         </div>
-        <div className="w-full md:w-1/4 h-full shadow-lg border-r border-white bg-gray-100">
-            Keranjang belanja di sini
-        </div>
-        </div>
-    }
-    sidebarSize={80}
- />
- </div>
- )
+    )
 }
