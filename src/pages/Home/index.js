dec57f230146db4f6ea760804d949d0849493d6a
diazadip <diaz_adip@yahoo.co.id>
2022-01-18 06:19:20 +0700

    JOSSS

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,24 +5,76 @@ import {
     LayoutSidebar,
     Responsive,
     CardProduct,
+    Pagination,
+    InputText,
+    Pill
 } from 'upkit';
 import TopBar from '../../components/TopBar';
 import menus from './menus';
-import { fetchProducts } from '../../features/Products/actions';
+import {
+    fetchProducts,
+    setPage, // (1) import `setPage`
+    goToNextPage, // (2) import `goToNextPage`
+    goToPrevPage, // (3) import `goToPrevPage`
+    setKeyword,
+    setCategory,
+    toggleTag
+} from '../../features/Products/actions';
+import BounceLoader from 'react-spinners/BounceLoader';
+import { tags } from './tags';
+
 export default function Home() {
     let dispatch = useDispatch();
     let products = useSelector(state => state.products);
     React.useEffect(() => {
         dispatch(fetchProducts());
-    }, [dispatch])
+    }, [dispatch, products.currentPage, products.keyword,
+        products.category, products.tags])
 
     return (
         <div>
             <LayoutSidebar
-                sidebar={<SideNav items={menus} verticalAlign="top" />}
+                sidebar={<SideNav
+                    items={menus}
+                    verticalAlign="top"
+                    active={products.category}
+                    onChange={category => dispatch(setCategory(category))}
+                />}
+
                 content={<div className="md:flex md:flex-row-reverse w-full mr-5 h-full min-h-screen">
                     <div className="w-full md:w-3/4 pl-5 pb-10">
                         <TopBar />
+                        <div className="w-full text-center mb-10 mt-5">
+                            <InputText
+                                fullRound
+                                value={products.keyword}
+                                placeholder="cari makanan favoritmu..."
+                                fitContainer
+                                onChange={e => {
+                                    dispatch(setKeyword(e.target.value))
+                                }}
+                            />
+                        </div>
+
+                        <div className="mb-5 pl-2 flex w-3/3 overflow-auto pb-5">
+                            {tags[products.category].map((tag, index) => {
+                                return <div key={index}>
+                                    <Pill
+                                        text={tag}
+                                        icon={tag.slice(0, 1).toUpperCase()}
+                                        isActive={products.tags.includes(tag)}
+                                        onClick={_ => dispatch(toggleTag(tag))}
+                                    />
+                                </div>
+                            })}
+                        </div>
+
+                        {products.status === 'process' && !products.data.length ?
+                            <div className="flex justify-center">
+                                <BounceLoader color="red" />
+                            </div>
+                            : null}
+
                         <Responsive desktop={3} items="stretch">
                             {products.data.map((product, index) => {
                                 return <div key={index} className="p-2">
@@ -36,6 +88,16 @@ export default function Home() {
                                 </div>
                             })}
                         </Responsive>
+                        <div className="text-center my-10">
+                            <Pagination
+                                totalItems={products.totalItems}
+                                page={products.currentPage}
+                                perPage={products.perPage}
+                                onChange={page => dispatch(setPage(page))}
+                                onNext={_ => dispatch(goToNextPage())}
+                                onPrev={_ => dispatch(goToPrevPage())}
+                            />
+                        </div>
                     </div>
 
                     <div className="w-full md:w-1/4 h-full shadow-lg border-r border-white bg-gray-100">
